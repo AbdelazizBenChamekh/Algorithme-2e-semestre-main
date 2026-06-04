0cbee221b3f8f5cac8d606257034c69a33467cb1
Abdelaziz Chamekh <108158902+AbdelazizBenChamekh@users.noreply.github.com>
2026-06-04 19:09:04 +0300

    Update task1.py

--- lab1/lab_1/src/task1.py
+++ lab1/lab_1/src/task1.py
@@ -1,28 +1,16 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
-file_path1 = os.path.abspath(os.path.join(os.path.dirname(__file__), "../txtf/input.txt"))
-file_path2 = os.path.abspath(os.path.join(os.path.dirname(__file__), "../txtf/output.txt"))
-
-from lab1.utils import read_input_file, write_output_file  
-
-def fractional_knapsack(n, W, items):
-    items.sort(key=lambda x: x[0]/x[1], reverse=True)
-    total_value = 0.0
-    for value, weight in items:
-        if W == 0:
-            break
-        fraction = min(weight, W)
-        total_value += fraction * (value / weight)
-        W -= fraction
-    return total_value
-
-
-n, W, items = read_input_file(file_path1)
-
-
-max_value = fractional_knapsack(n, W, items)
-
-
-write_output_file(file_path2, max_value)
+def collect_signatures(segments):
+    segments.sort(key=lambda x: x[1])  # сортировка по правому концу
+    points = []
+    i = 0
+    while i < len(segments):
+        x = segments[i][1]  # берём правый конец
+        points.append(x)
+        # пропускаем все отрезки, покрытые точкой x
+        while i < len(segments) and segments[i][0] <= x:
+            i += 1
+    return points
+n = int(input())
+segs = [list(map(int, input().split())) for _ in range(n)]
+pts = collect_signatures(segs)
+print(len(pts))
+print(*pts)
